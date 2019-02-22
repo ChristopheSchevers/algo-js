19a51ef90541ea67cf5f35d8ead7fe1a86387379
Christophe Schevers <christophe.schevers@gmail.com>
2019-02-22 16:47:00 +0100

    Added exercise

---
+++ 2.3.js
@@ -0,0 +1,13 @@
+let i = 1;
+
+while (i <= 100) {
+    i++;
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+/* for (i=1;i<=100;i++) {
+    if(i%2>0)continue;
+    console.log( i )
+} */
