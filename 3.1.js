91ece48eebd0ef459c249f5721f64fe0a228ab43
Christophe Schevers <christophe.schevers@gmail.com>
2019-02-25 13:52:20 +0100

    Added exercise

---
+++ 3.1.js
@@ -0,0 +1,7 @@
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [100, 101, 102];
+let sum1 = 0;
+let sum2 = 0;
+
+console.log(arr1.reduce((a, b) => a + b, 0));
+console.log(arr2.reduce((a, b) => a + b, 0));
