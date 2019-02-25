554efd0ddca33ecb08775f1e0410e1fba1185d32
Christophe Schevers <christophe.schevers@gmail.com>
2019-02-25 14:28:12 +0100

    Added exercise

---
+++ 3.4.js
@@ -0,0 +1,10 @@
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [100, 101, 102];
+
+// Minimum of array
+console.log(Math.min(...arr1));
+console.log(Math.min(...arr2));
+
+// Maximum of array
+console.log(Math.max(...arr1));
+console.log(Math.max(...arr2));
