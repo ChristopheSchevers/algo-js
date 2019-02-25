ee85053b05c5a955f8492c52a71d59db887b0f69
Christophe Schevers <christophe.schevers@gmail.com>
2019-02-25 15:02:11 +0100

    Added documentation to 4.2.js

--- 4.2.js
+++ 4.2.js
@@ -1,4 +1,5 @@
 // Function
+/* This function will return a random number between 1 and 10. First it will generate a number between 0 and 1 with random(). By multiplying it with 10 we make sure the options vary till 10. We need to add 1, otherwise the highest possible outcome would be 9. We need to floor the result to keep an integer. */
 function rand10() {
     return Math.floor((Math.random() * 10) + 1);
 }
