ab1895886906cca170aa4b606b789be0ecaed5af
Christophe Schevers <christophe.schevers@gmail.com>
2019-02-26 10:36:36 +0100

    Modified exercise

--- 2.2.js
+++ 2.2.js
@@ -2,12 +2,14 @@ const readlineSync = require("readline-sync");
 
 let min = Number(readlineSync.question("Please enter an minimum value. "));
 let max = Number(readlineSync.question("Please enter an maximum value. "));
-let current = Number(readlineSync.question("Please enter the current value. "));
 
 if (min > max) {
     console.log("Error: It appears you don't understand anything. Goodbye.");
-} else if ((current >= min) && (current <= max)) {
-    console.log(`Current value is ${current}.`);
 } else {
-    console.log("Current value is not situated between minimum and maximum value.");
-}
+    let current = Number(readlineSync.question("Please enter the current value. "));
+    if ((current >= min) && (current <= max)) {
+        console.log(`Current value is ${current}.`);
+    } else {
+        console.log("Current value is not situated between minimum and maximum value.");
+    }    
+} 
