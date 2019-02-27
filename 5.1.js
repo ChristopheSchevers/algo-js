2ad56ae026a2c08d9106ace9ac7af43c135696ac
Christophe Schevers <christophe.schevers@gmail.com>
2019-02-27 09:31:04 +0100

    Added exercise

--- 5.1.js
+++ 5.1.js
@@ -1,20 +1,27 @@
+// Enables readline-sync
 const readlineSync = require("readline-sync");
 
-
+// Function to create tv series object
 function askTvSerie (name, productionY, castMember) {
+    // Object to store data to
     tvSerie = {
         name: name,
         productionY: productionY,
-        castMember: [castMember]
+        castMember: castMember
     };
 
+    // Asks for data to store in assigned variables
     tvSerie.name = readlineSync.question("Please enter name of tv series. ");
     tvSerie.productionY = readlineSync.question("Please enter production year of tv series. ");
-    tvSerie.castMember = readlineSync.question("Please enter cast members of tv series. ");
+    // Will store output in one string
+    tvSerie.castMember = readlineSync.question("Please enter cast members of tv series. Seperate several cast members with a comma. ");
 
-    tvSerie.castMember.split(',');
+    // Will split the string in several array elements on every comma
+    tvSerie.castMember = tvSerie.castMember.split(',');
 
-    return tvSerie;
+    // Will return the created object in JSON format
+    return JSON.stringify(tvSerie, null, "     ");
 }
 
+// Calls and prints the result of askTvSerie() to the console
 console.log(askTvSerie());
